df81b834551c0ac62162066de7642c48569ae2fe
Jeff Schuman <jschuman@solutionstreet.com>
2024-03-05 08:50:55 -0500

    complete CRUD routes

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const db = require('./queries')
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -18,3 +20,9 @@ app.listen(port, () => {
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
